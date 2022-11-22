63edb8421c51b479a6a933c1fc0484b23c524c8e
EDunity <50490026+EDunity@users.noreply.github.com>
2022-11-22 23:27:03 +0900

    aaa

--- public/game.js
+++ public/game.js
@@ -1,23 +1,57 @@
 var Socket = io();
-var Movement = {};
+
+var Width_Window = 600;
+var Height_Window = 600;
+
+class Vector2
+{
+    constructor(x = 0, y = 0)
+    {
+        this.x = x;
+        this.y = y;
+    }
+};
+
+class Player
+{
+    constructor()
+    {
+        this.ID = -1;
+        this.PlayerName = "NoName";
+        this.RoomName = "Lobby";
+        this.Radius = 30;
+        this.Position = new Vector2((Width_Window - this.Radius), (Height_Window - this.Radius));
+        this.Movement = {};
+    }
+}
+
+var Player_ = new Player();
+
+function setup()
+{
+    createCanvas(Width_Window, Height_Window);
+
+    background(255);
+}
 
 //ゲーム開始時にGameStart関数を呼ぶ。
 Socket.on("connect", GameStart);
 
 function GameStart()
 {
-    //サーバにゲーム開始を表す"GameStart"メッセージを送る。
-    Socket.emit("GameStart", prompt("Please enter your name."));
+    Player_.ID = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+
+    Socket.emit("GameStart", Player_.ID);
 }
 
 $(document).on("keydown keyup", (_Event) => 
 {
     var KeyToCommand = 
     {
-        "w": "forward",
-        "s": "back",
-        "d": "right",
-        "a": "left",
+        "ArrowUp": "forward",
+        "ArrowDown": "back",
+        "ArrowRight": "right",
+        "ArrowLeft": "left",
     };
 
     var Command = KeyToCommand[_Event.key];
@@ -26,13 +60,67 @@ $(document).on("keydown keyup", (_Event) =>
     {
         if(_Event.type === "keydown")
         {
-            Movement[Command] = true;
+            Player_.Movement[Command] = true;
         }
         else if(_Event.type === "keyup")
         {
-            Movement[Command] = false;
+            Player_.Movement[Command] = false;
         }
 
-        Socket.emit("Movement", Movement);
+        Socket.emit("Movement", Player_.Movement);
     }
 });
+
+Socket.on("Update", function(_Players) 
+{
+    background(255);
+
+    textSize(25);
+
+    text("RN: " + Player_.RoomName, 5, 25);
+
+    textSize(12);
+
+    for(var i1 in _Players) 
+    {
+        if(_Players[i1].RoomName == Player_.RoomName)
+        {
+            if(_Players[i1].ID == Player_.ID)
+            {
+                fill(255, 0, 0);
+            }
+            else
+            {
+                fill(0, 0, 255);
+            }
+
+            text("ID: " + _Players[i1].ID, _Players[i1].Position.x + 15, _Players[i1].Position.y - 45);
+            text("PN: " + _Players[i1].PlayerName, _Players[i1].Position.x + 15, _Players[i1].Position.y - 30);
+            text("RN: " + _Players[i1].RoomName, _Players[i1].Position.x + 15, _Players[i1].Position.y - 15);
+            circle(_Players[i1].Position.x, _Players[i1].Position.y , _Players[i1].Radius);
+        }
+    }
+});
+
+// Socket.on("tmp", function() 
+// {
+//     console.log("yes");
+// });
+
+function Change_PlayerName()
+{
+    var PlayerName = document.getElementById("PlayerName").value;
+
+    Player_.PlayerName = PlayerName;
+
+    Socket.emit("Change_PlayerName", PlayerName);
+}
+
+function Change_RoomName()
+{
+    var RoomName = document.getElementById("RoomName").value;
+
+    Player_.RoomName = RoomName;
+
+    Socket.emit("Change_RoomName", RoomName);
+}
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var Express = require("express");//Webアプリを開発するためのフレームワーク
 var Http = require("http");//サーバを構築する上で必要な機能を提供するモジュール
 var Path = require("path");//ファイル名、ディレクトリ名、拡張子などのパス操作ができるモジュール
-var SocketIO = require("socket.io");//リアルタイム双方向通信をサポートしてくれるライブラリ
+var _SocketIO = require("socket.io");//リアルタイム双方向通信をサポートしてくれるライブラリ
 
 var App = Express();
-var Server = Http.Server(App);
-var IO = SocketIO(Server);
+var Server = Http.createServer(App);
+var IO = _SocketIO(Server);
 
 var Port = process.env.PORT || 3000;
 var FrameRate = 60;
@@ -13,40 +13,65 @@ var FrameRate = 60;
 var Width_Window = 600;
 var Height_Window = 600;
 
+class Vector2
+{
+    constructor(x = 0, y = 0)
+    {
+        this.x = x;
+        this.y = y;
+    }
+};
+
 class Player
 {
-    constructor(_UserName)
+    constructor()
     {
-        this.ID = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
-        this.Name = _UserName;
-        this.Width = 64;
-        this.Height = 64;
-        this.X = Math.random() * (Width_Window - this.Width);
-        this.Y = Math.random() * (Height_Window - this.Height);
+        this.ID = -1;
+        this.PlayerName = "NoName";
+        this.RoomName = "Lobby";
+        this.Radius = 30;
+        this.Position = new Vector2((Width_Window - this.Radius), (Height_Window - this.Radius));
         this.Movement = {};
     }
 };
 
+function Clamp(num, min, max)
+{
+    return Math.min(Math.max(num, min), max);
+}
+
 var Players = {};
 
 //クライアントが接続した時に呼ばれる関数
-IO.on("connection", function(Socket) 
+IO.on("connection", function(_Socket) 
 {
     var Player_ = null;
 
-    Socket.on("GameStart", function(_UserName) 
+    _Socket.on("GameStart", function(_ID) 
     {
-        if(_UserName == "" || _UserName == null)
-        {
-            _UserName = "Anonymous";
-        }
+        Player_ = new Player();
 
-        Player_ = new Player(_UserName);
+        Player_.ID = _ID;
 
         Players[Player_.ID] = Player_;
+
+        _Socket.join(Players[Player_.ID].RoomName);
     });
 
-    Socket.on("Movement", function(_Movement) 
+    _Socket.on("Change_PlayerName", function(_PlayerName) 
+    {
+        Players[Player_.ID].PlayerName = _PlayerName;
+    });
+
+    _Socket.on("Change_RoomName", function(_RoomName) 
+    {
+        _Socket.leave(Players[Player_.ID].RoomName);
+
+        _Socket.join(_RoomName);
+        Players[Player_.ID].RoomName = _RoomName;
+    });
+
+    _Socket.on("Movement", function(_Movement) 
     {
         if(Player_ != null)
         {
@@ -54,7 +79,7 @@ IO.on("connection", function(Socket)
         }
     });
 
-    Socket.on("disconnect", () => 
+    _Socket.on("disconnect", () => 
     {
         if(Player_ != null)
         {
@@ -72,23 +97,26 @@ setInterval(function()
     {
         if(Players[i1].Movement.forward)
         {
-            Players[i1].Y -= 5;
+            Players[i1].Position.y -= 5;
         }
         if(Players[i1].Movement.back)
         {
-            Players[i1].Y += 5;
+            Players[i1].Position.y += 5;
         }
         if(Players[i1].Movement.right)
         {
-            Players[i1].X += 5;
+            Players[i1].Position.x += 5;
         }
         if(Players[i1].Movement.left)
         {
-            Players[i1].X -= 5;
+            Players[i1].Position.x -= 5;
         }
+
+        Players[i1].Position.x = Clamp(Players[i1].Position.x, 0, Width_Window);
+        Players[i1].Position.y = Clamp(Players[i1].Position.y, 0, Height_Window);
     }
 
-    IO.sockets.emit("state", Players);
+    IO.sockets.emit("Update", Players);
 
 }, 1000 / FrameRate);
 
